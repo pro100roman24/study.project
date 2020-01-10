7531813abb523fde9854e18b45ea197bcacc800e
pro100roman24 <pro100roman242424@gmail.com>
2020-01-10 15:49:25 +0300

    ver2

--- 2.php
+++ 2.php
@@ -5,28 +5,33 @@ if (mysqli_connect_errno()) {
     printf("Не удалось подключиться: %s\n", mysqli_connect_error());
     exit();
 }
-$query = 'SELECT * FROM tankstable';
+$query = 'SELECT * FROM tanks';
 $table = '<table><tr><th>Название</th><th>Нация</th><th>Тип</th><th>Наличие башни</th><th>Живучесть</th></tr>';
 $tr = '<tr>';
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 if (!empty($result)) {
-    $arrData  = [];
+    $arrData = [];
     while ($row = mysqli_fetch_assoc($result)) {
         $arrData[] = $row;
     }
 }
 // теперь с помощью foreach перебираешь массив
-echo $table;
-foreach ($arrData as $line => $rows) {
-    echo $tr;
+//echo $table;
+foreach ($arrData as $key => $rows) {
+    //echo $tr;
+    $table .= '<tr>';
     foreach ($rows as $colName => $value) {
         if ($colName != 'id') {
-            if ($colName = 'Durability' and $value > 100)
-                echo "<td class=redStyle>$value</td>";
-            else echo "<td>$value</td>";
+            if ($colName = 'Durability' and $value > 100) {
+                $table .= "<td class=redStyle>$value</td>";
+            } else {
+                $table .= "<td>$value</td>";
+            }
+
         }
     }
 }
+echo $table;
 //звони
 //записываешь всё в переменную , а потом делаешь одно ечо
 /// сам ещё поковыряй, потом исправлю, сейчас очень неудобно
