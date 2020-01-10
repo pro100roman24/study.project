591d39b205ff57f20411e8270722f45ad1e5529a
pro100roman24 <pro100roman242424@gmail.com>
2020-01-10 18:00:21 +0300

    ver3

--- 2.php
+++ 2.php
@@ -1,13 +1,12 @@
 <link rel="stylesheet" type="text/css" href="Style.css"></link>
 <?php
-$link = mysqli_connect('localhost', 'admin', 'Resto01Test', 'php_db') or die ('Нет связи с Базой Данных');
+$link = mysqli_connect('localhost', 'admin', 'Resto01#Test', 'php_db') or die ('Нет связи с Базой Данных');
 if (mysqli_connect_errno()) {
     printf("Не удалось подключиться: %s\n", mysqli_connect_error());
     exit();
 }
 $query = 'SELECT * FROM tanks';
 $table = '<table><tr><th>Название</th><th>Нация</th><th>Тип</th><th>Наличие башни</th><th>Живучесть</th></tr>';
-$tr = '<tr>';
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 if (!empty($result)) {
     $arrData = [];
@@ -15,10 +14,7 @@ if (!empty($result)) {
         $arrData[] = $row;
     }
 }
-// теперь с помощью foreach перебираешь массив
-//echo $table;
 foreach ($arrData as $key => $rows) {
-    //echo $tr;
     $table .= '<tr>';
     foreach ($rows as $colName => $value) {
         if ($colName != 'id') {
@@ -32,8 +28,4 @@ foreach ($arrData as $key => $rows) {
     }
 }
 echo $table;
-//звони
-//записываешь всё в переменную , а потом делаешь одно ечо
-/// сам ещё поковыряй, потом исправлю, сейчас очень неудобно
-/// //PSR-1, PSR-2
-//?>
+?>
