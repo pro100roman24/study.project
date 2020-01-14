348711a169da6c6a3d132dd085f8976e675cb012
pro100roman24 <pro100roman242424@gmail.com>
2020-01-14 11:02:15 +0300

    ver9

--- Project1.php
+++ Project1.php
@@ -6,39 +6,42 @@ $user = $iniArray['user'];
 $pass = $iniArray['password'];
 $db = $iniArray['database'];
 $link = mysqli_connect($host, $user, $pass, $db) or die ('Нет связи с Базой Данных');
-//$link = mysqli_connect($ini_array['host'], $ini_array['user'], $ini_array['password'], $ini_array['database']) or die ('Нет связи с Базой Данных');
 if (mysqli_connect_errno()) {
     printf("Не удалось подключиться: %s\n", mysqli_connect_error());
     exit();
 }
-$tab = 'tanks2';
+$tab = 'tanks1';
 $table = '<table><tr><th>Название</th><th>Нация</th><th>Тип</th><th>Наличие башни</th><th>Живучесть</th></tr>';
 $querySelect = "SELECT * FROM $tab";
 $queryCreate = "create table IF NOT EXISTS $tab (id integer not null auto_increment primary key, tank varchar(25),  nation varchar(25), type varchar(25), turret varchar(25), durability integer)";
 $queryInsert = "INSERT INTO $tab (tank, nation, type, turret, durability) VALUES ('".$iniArray['tank']."', '". $iniArray['nation'] . "', '". $iniArray['type'] ."', '".$iniArray['turret']."', '".$iniArray['Durability']."')";
-if (mysqli_query($link, $querySelect)!==false){
-    $test="Таблица $tab существует";
-    echo $test;
-    $result = mysqli_query($link, $querySelect);
+if (mysqli_query($link, $querySelect)!==false and mysqli_num_rows(mysqli_query($link, $querySelect))==0){ //если select вернул не пустое значение и в таблице 0 строк
+	mysqli_query($link, $queryInsert);                                                                //тогда заполняем пустую таблицу нашей строкой из ini файла
+    $result = mysqli_query($link, $querySelect);                                                     //результат select-а кладем в result
+    $text = "Таблица $tab была пустой и заполнена параметрами по умолчанию";
 }
-else{
-    $test="Таблица $tab не существовала и была создана с параметрами по умолчанию";
-    echo $test;
-    mysqli_multi_query($link, $queryCreate);
-    mysqli_query($link, $queryInsert);
-    $result = mysqli_query($link, $querySelect);
+elseif (mysqli_query($link, $querySelect)!==false){                    //если select вернул не пустое значение, но в таблице уже не 0 строк
+    $text = "Таблица $tab существует ";
+    $result = mysqli_query($link, $querySelect);                      //делаем select в result
 }
-    if (!empty($result)) {
+else{                                                               //иначе таблицы не существует
+    $text = "Таблица $tab не существовала и была создана с параметрами по умолчанию";
+    mysqli_query($link, $queryCreate);                           //создаем таблицу
+    mysqli_query($link, $queryInsert);                           //заполняем строкой из ini файла
+    $result = mysqli_query($link, $querySelect);                 //результат select-а кладем в result
+}
+
+    if (!empty($result)) {                  //это наверно надо убрать, result не может быть пустым в любом случае
     $arrData = [];
     while ($row = mysqli_fetch_assoc($result)) {
-        $arrData[] = $row;
+        $arrData[] = $row;                      //из всех результатов собираем массив
     }
 }
-foreach ($arrData as $rows) {
+foreach ($arrData as $rows) {                 //пересобираем массив $arrData в таблицу $table для вывода
     $table .= '<tr>';
     foreach ($rows as $colName => $value) {
         if ($colName != 'id') {
-            if ($colName = 'Durability' and $value > 100) {
+            if ($colName = 'Durability' and $value > 100) {    //подсвечиваим значения >100
                 $table .= "<td class=redStyle>$value</td>";
             } else {
                 $table .= "<td>$value</td>";
@@ -47,5 +50,6 @@ foreach ($arrData as $rows) {
     }
 }
 echo $table;
+echo $text;
 mysqli_close($link);
 ?>
